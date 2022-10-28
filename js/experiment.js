2c4f2d0e3dd340121ba0bee4294d457b0c41c621
vikas148 <vb334531@gmail.com>
2022-10-28 21:28:45 +0530

    experiments

---
+++ js/experiment.js
@@ -0,0 +1,21 @@
+console.log("hello from experiment");
+
+const monthlyrent =500;
+const yearlyrent=monthlyrent*12;
+console.log(yearlyrent);
+
+
+console.log("Hello world !");
+
+
+const firstname="vikas";
+const lastname="bairwa";
+
+const sentance="hello " + firstname + "how are you !";
+
+const sentancewith_Template =`hello  ${firstname}  ${lastname} how are you`
+
+
+console.log(sentance);
+
+console.log(sentancewith_Template);
