ee6a8cccf8442e35915bbc4a50a75584334913e9
vikas148 <vb334531@gmail.com>
2022-10-28 21:28:34 +0530

    controlls

---
+++ js/controlflow.js
@@ -0,0 +1,19 @@
+const skyBlue=true;
+
+if (skyBlue) {
+    console.log("The sky is blue");
+}
+
+else{
+    console.log('sky is no blue !');
+}
+
+
+
+if (2+2==4) {
+    console.log("oh thnk god my math is still working");
+}
+
+else{
+    console.log("ohh,its panic");
+}
