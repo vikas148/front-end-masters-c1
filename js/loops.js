360eac47d59632cba2a6e0dbe2f5e6486ac467d1
vikas148 <vb334531@gmail.com>
2022-10-29 23:31:07 +0530

    loops

---
+++ js/loops.js
@@ -0,0 +1,33 @@
+let friendsAtYourParty=0;
+
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// friendsAtYourParty=friendsAtYourParty+1;
+// console.log(friendsAtYourParty);
+
+
+while (friendsAtYourParty<10) {
+
+    friendsAtYourParty=friendsAtYourParty+1;
+    console.log('another friend joins your party');
+
+}
+
+console.log(friendsAtYourParty);
+
+
+
+let greet=1;
+
+for(let i=0;i<10;i++)
+{
+  greet++;
+}
+console.log(greet);
