7e77605f5419f14ca0669a7b99a32ff942fd4d6b
vikas148 <vb334531@gmail.com>
2022-10-31 00:25:39 +0530

    context

---
+++ functions/context.js
@@ -0,0 +1,22 @@
+const me = {
+    name: {
+      first: "Brian",
+      last: "Holt",
+    },
+    location: {
+      streetNumber: 500,
+      street: "Fakestreet",
+      city: "Seattle",
+      state: "WA",
+      zipCode: 55555,
+      country: "USA",
+    },
+
+    getAddress(){
+        return `${this.name.first} ${this.name.last} ${this.location.streetNumber} ${this.location.street}
+        ${this.location.city}, ${this.location.state} ${this.location.zipCode}
+        ${this.location.country}`
+    },
+}
+
+console.log(me.getAddress());
