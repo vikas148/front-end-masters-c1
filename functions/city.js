1e1e40f15271adb915bbfc12744b5ed24896a796
vikas148 <vb334531@gmail.com>
2022-10-30 21:56:44 +0530

    cityjs

---
+++ functions/city.js
@@ -0,0 +1,10 @@
+const myHomeCity ="Seattle";
+const myHomeState= "Washington";
+const myHomeCountry="USA";
+
+function loOUtYourHome(City,State,Country) {
+    console.log(`You are from ${City} ,${State} , ${Country} `);
+}
+
+loOUtYourHome(myHomeCity,myHomeState,myHomeCountry);
+loOUtYourHome("jaipur","rajasthan","INdia")
