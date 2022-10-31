051b211a57a20ee112a6956ed48d589beb63b48d
vikas148 <vb334531@gmail.com>
2022-11-01 00:34:02 +0530

    apijs

---
+++ talking to sercer/api2.js
@@ -0,0 +1,23 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const doggos = document.getElementById("dog-target2");
+
+function addNewDoggo() {
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function (response) {
+    const processingPromise = response.json(); // json instead of text
+    return processingPromise;
+  })
+    .then(function (processedResponse) {
+    // we get to skip this line since it'll already be an object
+    // const dogObject = JSON.parse(processedResponse);
+
+    const img = document.createElement("img");
+    img.src = processedResponse.message;
+    img.alt = "Cute doggo";
+    doggos.appendChild(img);
+  });
+}
+
+document.getElementById("dog-btn2").addEventListener("click", addNewDoggo);
